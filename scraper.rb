97a72423e7021c59d81c32c40117ca783aca2d1f
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-07-06 11:24:30 +0100

    Finish scrapping all the data from a member

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,10 @@ url   = 'https://morph.io/documentation/examples/australian_members_of_parliamen
 page  = agent.get(url)
 
 member = {
-  title: page.at('.title').at('a').inner_text
+  title:      page.at('.title').at('a').inner_text,
+  electorate: page.search('dd')[0].inner_text << ", " <<
+              page.search('dd')[1].inner_text,
+  url:        page.at('.title a').attr('href')
 }
 
 p member
