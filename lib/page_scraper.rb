0727e8f8d33b544581b29eba149969971cc299f2
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-07-07 22:21:29 +0100

    Return the total number of pages from pagination and refactor scraper

--- lib/page_scraper.rb
+++ lib/page_scraper.rb
@@ -1,13 +1,18 @@
 class PageScraper
 
   def member_at(index, page)
-    all_members(page)[index]
+    all_members_in(page)[index]
   end
 
-  def all_members(page)
-    page.at('.search-filter-results').search('li').map { |li|
+  def all_members_in(page)
+    page.at(".search-filter-results").search("li").map { |li|
       member_structure(li)
     }
+  end
+
+  def total_pages_in(page)
+    last = last_link(page)
+    last.nil? ? 1 : page_number_from(last)
   end
 
   private
@@ -16,11 +21,25 @@ class PageScraper
 
   def member_structure(li)
     {
-      title:      li.at('.title a').inner_text,
-      electorate: li.search('dd')[0].inner_text,
-      party:      li.search('dd')[1].inner_text,
-      url:        li.at('.title a').attr('href')
+      title:      li.at(".title a").inner_text,
+      electorate: li.search("dd")[0].inner_text,
+      party:      li.search("dd")[1].inner_text,
+      url:        li.at(".title a").attr("href")
     }
+  end
+
+  def last_link(page)
+    page.at(".results-pagination").search("li a").select { |a|
+      is_pagination_link_and_last_link?(a)
+    }.first
+  end
+
+  def is_pagination_link_and_last_link?(a)
+    a.attr("href").include?("?page=") && a.inner_text.downcase.include?("last")
+  end
+
+  def page_number_from(a)
+    a.attr("href").split("=").last.to_i
   end
 
 end
--- spec/page_scraper_spec.rb
+++ spec/page_scraper_spec.rb
@@ -1,25 +1,52 @@
 describe("PageScraper") do
 
-  let(:page_scraper) {PageScraper.new}
-  let(:page)         {Mechanize.new.get(TEST_URL)}
+  let(:scraper) {PageScraper.new}
+  let(:page)    {page_at(TEST_URL)}
 
-  it "returns the title of a member" do
-    expect(page_scraper.member_at(0, page)[:title]).to eq("The Hon Ian Macfarlane MP")
+  describe "when there are search results" do
+    it "finds the title of a member" do
+      expect_field_to_be(:title, "The Hon Ian Macfarlane MP")
+    end
+
+    it "finds the electorate of a member" do
+      expect_field_to_be(:electorate, "Groom, Queensland")
+    end
+
+    it "finds the party of a member" do
+      expect_field_to_be(:party, "Liberal Party of Australia")
+    end
+
+    it "finds the url of a member" do
+      expect_field_to_be(:url, "http://www.aph.gov.au/Senators_and_Members/Parliamentarian?MPID=WN6")
+    end
+
+    it "reads all the members" do
+      expect(scraper.all_members_in(page).size).to eq(50)
+    end
+
+    def expect_field_to_be(field, value)
+      expect(scraper.member_at(0, page)[field]).to eq(value)
+    end
   end
 
-  it "returns the electorate of a member" do
-    expect(page_scraper.member_at(0, page)[:electorate]).to eq("Groom, Queensland")
+  describe "when there is pagination" do
+    it "finds the total number of pages" do
+      expect(scraper.total_pages_in(page)).to eq(3)
+    end
+
+    it "detects single page if there are no pagination links" do
+      page = page_at("#{TEST_URL}/nopaginationlinks.html")
+      expect(scraper.total_pages_in(page)).to eq(1)
+    end
+
+    it "needs a link to the last page to find the total pages" do
+      page = page_at("#{TEST_URL}/nolinktolastpage.html")
+      expect(scraper.total_pages_in(page)).to eq(1)
+    end
   end
 
-  it "returns the party of a member" do
-    expect(page_scraper.member_at(0, page)[:party]).to eq("Liberal Party of Australia")
+  def page_at(url)
+    PageFetcher.new.page_at(url)
   end
 
-  it "returns the url of a member" do
-    expect(page_scraper.member_at(0, page)[:url]).to eq("http://www.aph.gov.au/Senators_and_Members/Parliamentarian?MPID=WN6")
-  end
-
-  it "reads all the members" do
-    expect(page_scraper.all_members(page).size).to eq(50)
-  end
 end
